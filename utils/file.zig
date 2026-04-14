ea64a9ca9915cd5f5bd51edbff58a059b50c0521
Romain Crestey <rcrestey@gmail.com>
2026-04-14 22:35:52 +0200

    utils: read_by_line

--- utils/file.zig
+++ utils/file.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const Allocator = std.mem.Allocator;
 const File = std.Io.File;
 
 pub fn read_file(io: std.Io, buf: []u8, filename: []const u8) (File.OpenError || File.ReadPositionalError)!void {
@@ -10,17 +11,18 @@ pub fn read_file(io: std.Io, buf: []u8, filename: []const u8) (File.OpenError ||
     _ = try file.readPositionalAll(io, buf, 0);
 }
 
-pub fn read_by_line(init: std.process.Init) !void {
-    const io = init.io;
+pub fn read_by_line(io: std.Io, filename: []const u8, allocator: Allocator) ![][]const u8 {
+    const cwd = std.Io.Dir.cwd();
 
-    const cwd: std.Io.Dir = std.Io.Dir.cwd();
-
-    const file: std.Io.File = try cwd.openFile(io, "test.txt", .{});
+    const file = try cwd.openFile(io, filename, .{});
     defer file.close(io);
 
-    const file_buffer: []u8 = undefined;
-    var file_reader = file.reader(io, file_buffer);
+    const buf: []u8 = try allocator.alloc(u8, 10_000);
+
+    var file_reader = file.reader(io, buf);
     const reader = &file_reader.interface;
+
+    var lines = std.ArrayList([]const u8).empty;
 
     while (true) {
         const line = reader.takeDelimiter('\n') catch |e| {
@@ -29,8 +31,10 @@ pub fn read_by_line(init: std.process.Init) !void {
                 break;
             }
             return e;
-        } orelse return;
+        } orelse break;
 
-        std.debug.print("{s}\n", .{line});
+        try lines.append(allocator, line);
     }
+
+    return lines.items;
 }
